992791184a94b53d864fc4585868e777ef5ded6c
Marat Zhurm <zmhan.box@gmail.com>
2018-07-03 00:28:04 +0500

    Webpack init

---
+++ config/webpack.dev.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/main.js' // точка входа
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name]-bundle.js',
+    publicPath: '/' // общий путь для бандла 
+  },
+  devServer: {
+    contentBase: 'dist',
+    overlay: true // вывод ошибок в браузер
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          // {
+          //   loader: "sass-loader"
+          // },
+          { 
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader', // загрузить файлы
+            options: {
+              name: '[name].html' 
+            }
+          },
+          {
+            loader: 'extract-loader' // найти пути
+          },
+          {
+            loader: 'html-loader', // снизу вверх 
+            options: {
+              attrs: ['img:src']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|gif|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
